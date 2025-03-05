b74223fb0deb33c542dd4f3aea3f0b0c63db9fa9
9600bauds <blasaladio3@gmail.com>
2025-03-05 14:27:07 -0300

    Overflow for react-menu

--- src/components/BuildingSelector.tsx
+++ src/components/BuildingSelector.tsx
@@ -29,6 +29,7 @@ const BuildingSelector = ({ selectSubcategory }: BuildingSelectorProps) => {
         return (
           <Menu
             key={category.displayName}
+            overflow="auto"
             menuButton={
               <MenuButton className="px-4 py-2 bg-gray-100 hover:bg-gray-200 rounded">
                 {category.symbol}
