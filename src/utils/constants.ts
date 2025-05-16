1caa383979f8fdb5eb048a979b3c00954e85521a
9600bauds <blasaladio3@gmail.com>
2025-05-16 01:18:46 -0300

    Fixes lint freaking out over this being a string?

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -64,5 +64,5 @@ export const MIN_DESIRABILITY_COLOR = -10;
 export const MAX_DESIRABILITY_COLOR = 50;
 
 export const URL_STATE_INDEX = 'buildings';
-export const LOCALSTORAGE_KEY_SEEN_INSTRUCTIONS =
+export const LOCALSTORAGE_KEY_SEEN_INSTRUCTIONS: string =
   'desirabilityPlanner_hasSeenInstructions';
